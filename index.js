7b651a393d50f9b438a5fc12e68df871fa68ac06
Brian Woodward <brian.woodward@gmail.com>
2015-12-21 13:27:10 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * gitbot <https://github.com/doowb/gitbot>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
